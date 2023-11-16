24866e9a8f51d21d4159cbbf232f2395d493dd60
Ari Setiawan <ari_22510010@stimata.ac.id>
2023-11-16 21:29:27 +0700

    update biodata diri
    update layout dan baris kode

--- app/src/main/java/com/example/biodatadiri/MainActivity.kt
+++ app/src/main/java/com/example/biodatadiri/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.biodatadiri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.biodatadiri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnSave.setOnClickListener{ goToProfilActivity()}
     }
+
+    private fun goToProfilActivity() {
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
+    }
+    
 }
